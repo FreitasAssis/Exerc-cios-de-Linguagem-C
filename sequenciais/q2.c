7910bc9d9e1556ce2379c9b641497d7f672d7d09
Luiz <luizvpc@gmail.com>
2019-04-01 17:01:03 -0300

    Update q2.c

--- sequenciais/q2.c
+++ sequenciais/q2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 int main(int argc, char ** argv) {
+  int x;
   printf("Digite um número inteiro");
   scanf("%d\n", &x);
   printf(x%5==0?"O valor é múltiplo de 5":"O valor não é múltiplo de 5");
